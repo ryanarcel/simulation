2ef75cc7c195f5949b0821d2630c2d1f18df7432
ryanarcel <ryan.galendez@go.team>
2024-02-26 10:22:09 +0800

    first commit

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h1>Hello {{name}}!</h1>
+        <a :href="route('second')">Go to Route</a>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
