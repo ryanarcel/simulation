1cda11f10ae952ccc5a3ccd76cd9eb3b7c88461a
ryanarcel <ryan.galendez@go.team>
2024-02-29 10:26:19 +0800

    Setup authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -9,32 +11,28 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return Inertia::render('Users/Index', [
-        'name' => 'Chong'
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::get('/options-api', function () {
-    return Inertia::render('Users/Options', [
-        'name' => 'Chong',
-        'birthdate' => 'September 11, 1992'
-    ]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/composition-api', function () {
-    return Inertia::render('Users/Composition', [
-        'name' => 'Chong',
-        'birthdate' => 'September 11, 1992'
-    ]);
-});
-
-
-Route::get('/results', [App\Http\Controllers\DefinitionController::class, 'show']);
+require __DIR__.'/auth.php';
