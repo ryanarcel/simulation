df01853970fb84a24f2805699b8ec0a637f445cd
ryanarcel <ryan.galendez@go.team>
2024-03-02 14:13:22 +0800

    feat: styling card layout

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,27 +3,24 @@
 
   <AuthenticatedLayout>
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+      <h2 class="font-semibold text-xl text-gray-200 leading-tight ">Dashboard</h2>
     </template>
 
     <form @submit.prevent="submit" class="mt-10 flex flex-col items-center">
       <div class="mx-auto block w-3/5">
         <div class="mt-2">
-          <input v-model="form.search" type="text" name="search" id="search" autocomplete="search"
+          <input v-model="form.search" type="text" name="search" id="search"
             placeholder="Search your movie"
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            class="block w-3/5 mx-auto rounded-md border-0 py-5 text-gray-900 shadow-sm ring-1 ring-inset lg:text-lg ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
-      <button class="bg-gray-100 text-greenBlue font-bold py-2 px-4 rounded mt-5" type="submit">
+      <button class="bg-pumpkin text-gray-700 lg:text-lg font-black py-2 px-4 rounded mt-5" type="submit">
         Search Movie
       </button>
     </form>
 
     <div class="w-3/5 mx-auto">
-      <div v-if="searchResults" class="grid grid-cols-3 gap-3 mt-10">
-        <!-- <li v-for="movie in searchResults">
-          {{ typeof movie }}
-        </li> -->
+      <div v-if="searchResults" class="grid lg:grid-cols-4 mt-10">
         <Movie v-for="movie in searchResults" :movie="movie"/>
       </div>
     </div>
